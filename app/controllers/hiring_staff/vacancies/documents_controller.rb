e5125bbde822bc1b35ba0752387c77da39607721
cpjmcquillan <connor.mcquillan17@gmail.com>
2020-02-07 14:41:35 +0000

    Hiring staff must select 'yes' on supporting documents page to upload documents

--- app/controllers/hiring_staff/vacancies/documents_controller.rb
+++ app/controllers/hiring_staff/vacancies/documents_controller.rb
@@ -1,9 +1,10 @@
 class HiringStaff::Vacancies::DocumentsController < HiringStaff::Vacancies::ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:create]
   before_action :school, :redirect_unless_vacancy_session_id, only: %i[new create]
 
   def new
-    # WIP
+    unless session[:vacancy_attributes]['supporting_documents'] == 'yes'
+      redirect_to supporting_documents_school_job_path
+    end
   end
 
   def create
--- spec/features/hiring_staff_can_upload_documents_spec.rb
+++ spec/features/hiring_staff_can_upload_documents_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.feature 'Hiring staff can upload documents to a vacancy' do
+  let(:feature_enabled?) { true }
+  let(:session_id) { SecureRandom.uuid }
   let(:school) { create(:school) }
   let!(:pay_scales) { create_list(:pay_scale, 3) }
   let!(:subjects) { create_list(:subject, 3) }
@@ -19,25 +21,33 @@ RSpec.feature 'Hiring staff can upload documents to a vacancy' do
   end
 
   before do
-    allow(ENV).to receive(:[])
-    allow(ENV).to receive(:[]).with('FEATURE_UPLOAD_DOCUMENTS').and_return(true)
+    allow(UploadDocumentsFeature).to receive(:enabled?).and_return(feature_enabled?)
     stub_hiring_staff_auth(urn: school.urn)
+
+    visit new_school_job_path
+    fill_in_job_specification_form_fields(vacancy)
+    click_on 'Save and continue'
   end
 
-  context 'when a user wants to upload documents to a vacancy' do
+  context "without 'yes' previously selected for supporting documents" do
+    scenario 'redirects to step 2, supporting documents' do
+      visit documents_school_job_path
+      expect(page.current_path).to eq(supporting_documents_school_job_path)
+    end
+  end
+
+  context "with 'yes' previously selected for supporting documents" do
     before do
-      visit new_school_job_path
-      fill_in_job_specification_form_fields(vacancy)
+      visit supporting_documents_school_job_path
+      fill_in_supporting_documents_form_fields(vacancy)
       click_on 'Save and continue'
     end
 
-    it 'displays upload a file text' do
-      visit documents_school_job_path
+    scenario 'displays upload a file text' do
       expect(page).to have_content('Upload a file')
     end
 
     scenario 'hiring staff can select a file for upload' do
-      visit documents_school_job_path
       page.attach_file('upload', Rails.root.join('spec/fixtures/files/blank_job_spec.pdf'))
       expect(page.find('#upload').value).to_not be nil
     end
